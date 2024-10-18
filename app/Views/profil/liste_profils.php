c102a71043a46853b599558e5b7eccda55bd09c4
BondouxBastian82 <luminou.bondoux@gmail.com>
2024-10-18 16:29:45 +0200

    Profil

---
+++ app/Views/profil/liste_profils.php
@@ -0,0 +1,50 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('contenu') ?>
+<html>
+<!-- <link rel="stylesheet" type="text/css" href="css/main.css" /> -->
+
+<body>
+    <a href=<?= url_to("create_profil") ?>>Ajouter profil</a>
+
+    <?php
+    foreach ($listeProfils as $profil) {
+    ?>
+        <p>
+            <?php
+
+            echo $profil['INTITULE_PROFIL']
+            ?>
+            <form method="post" action=" <?= url_to('suppr_profil') ?>">
+                <input id="ID_PROFIL" name="ID_PROFIL" type="hidden" value="<?= $profil['ID_PROFIL']?>">
+                <input type="submit" value="supprimer">
+            </form>
+            
+
+    </p><br>
+<?php
+
+
+    }
+    ?>
+</body>
+
+</html>
+
+<?= $this->endSection() ?>
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
